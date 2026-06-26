5bffa4fa9855d66d06c39e0f2df1eb9ddebf7f13
ErikStumpfOgliari <errike@outlook.com>
2026-06-26 01:41:34 -0300

    fix: force pg and sequelize as webpack externals para Vercel serverless

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ['sequelize', 'pg', 'pg-hstore', 'pg-pool', 'pg-protocol'],
+    serverComponentsExternalPackages: [
+      'sequelize', 'pg', 'pg-hstore', 'pg-pool', 'pg-protocol',
+      'pg-connection-string', 'sequelize-pool', 'wkx', 'validator',
+      'semver', 'toposort-class', 'moment', 'moment-timezone',
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      const externals = ['pg', 'pg-hstore', 'pg-native', 'pg-pool', 'pg-protocol',
+        'pg-connection-string', 'sequelize', 'sequelize-pool'];
+      config.externals = [
+        ...(Array.isArray(config.externals) ? config.externals : [config.externals]),
+        ({ request }, callback) => {
+          if (externals.includes(request)) return callback(null, `commonjs ${request}`);
+          callback();
+        },
+      ];
+    }
+    return config;
   },
   images: {
     remotePatterns: [{ protocol: 'https', hostname: '**' }],
